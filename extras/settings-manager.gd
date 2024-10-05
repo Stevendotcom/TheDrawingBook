edac998e05810c55a91510d92553d919d0b10547
Alex Gallardo <gallardonicolas.8100@outlook.com>
2024-10-05 11:23:11 -0300

    add volume

--- extras/settings-manager.gd
+++ extras/settings-manager.gd
@@ -1,11 +1,28 @@
 extends Node
 
+enum axis {
+	width,
+	height,
+}
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var volume: int = 5
+var screen_width: int = 1366
+var screen_height: int = 768
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+const screen_sizes = {
+	"large" = [1366, 768],
+	"mid" = [1280, 720],
+	"small" = [1024, 576],
+}
+
+const max_vol: int = 10
+
+func change_vol(newVol: int) -> void:
+	volume = newVol
+
+func change_screen_size(screen_size: String) -> void:
+	if screen_sizes.find_key(screen_size):
+		screen_width = screen_sizes[screen_size][axis.width]
+		screen_height = screen_sizes[screen_size][1]
+	
