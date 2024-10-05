32d7981b7bae4f0b42cfeffa6371467fc3c69297
Alex Gallardo <gallardonicolas.8100@outlook.com>
2024-10-05 16:51:44 -0300

    add delete method

--- extras/entity-manager.gd
+++ extras/entity-manager.gd
@@ -3,12 +3,18 @@ extends Node
 const MONSTER = preload("res://entities/monster.tscn")
 
 var amount_entities: int = 0
-var entities: Array = []
+var entities: Array = [null]
 const amount_entities_max: int = 15
 
 func new_monster(this_node: Node) -> void:
 	if amount_entities < amount_entities_max:
-		var instance = MONSTER.instantiate()
+		var instance : Node = MONSTER.instantiate()
 		entities.push_back(instance)
 		this_node.add_child(instance)
 		amount_entities += 1
+
+func delete_monster(this_node: Node, monster_id: int) -> void:
+	if monster_id < amount_entities_max and monster_id >= 0 and entities[monster_id] != null:
+		entities[monster_id].free()
+		entities[monster_id] = null
+		amount_entities -= 1
