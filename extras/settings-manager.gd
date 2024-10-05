480aa9d1185280f7f8860b8173b7deb3b486ea0d
Alex Gallardo <gallardonicolas.8100@outlook.com>
2024-10-05 12:16:56 -0300

    add fullscreen option

--- extras/settings-manager.gd
+++ extras/settings-manager.gd
@@ -8,7 +8,7 @@ enum axis {
 var volume: int = 5
 var screen_width: int = 1366
 var screen_height: int = 768
-
+var fullscreen: bool = false
 
 const screen_sizes = {
 	"large" = [1366, 768],
@@ -25,4 +25,7 @@ func change_screen_size(screen_size: String) -> void:
 	if screen_sizes.find_key(screen_size):
 		screen_width = screen_sizes[screen_size][axis.width]
 		screen_height = screen_sizes[screen_size][1]
-	
+
+
+func toggle_fullscreen() ->void:
+	fullscreen = !fullscreen
