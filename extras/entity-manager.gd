608d1d69fae28b452be4a35b17112fc5b3a6b210
Alex Gallardo <gallardonicolas.8100@outlook.com>
2024-10-05 17:07:52 -0300

    add merge monster

--- extras/entity-manager.gd
+++ extras/entity-manager.gd
@@ -14,7 +14,18 @@ func new_monster(this_node: Node) -> void:
 		amount_entities += 1
 
 func delete_monster(this_node: Node, monster_id: int) -> void:
-	if monster_id < amount_entities_max and monster_id >= 0 and entities[monster_id] != null:
+	if in_range(monster_id) and entities[monster_id] != null:
 		entities[monster_id].free()
 		entities[monster_id] = null
 		amount_entities -= 1
+
+func merge_monster(this_node: Node, monster_id_a: int, monster_id_b: int):
+	if in_range(monster_id_a) and in_range(monster_id_b) and entities[monster_id_a] != null and entities[monster_id_b] != null:
+		if entities[monster_id_a].level == entities[monster_id_b].level:
+			upgrade_monster(this_node, monster_id_a)
+			delete_monster(this_node, monster_id_b)
+		
+
+
+func in_range(monster_id: int) -> bool:
+	return monster_id < amount_entities_max and monster_id >= 0
