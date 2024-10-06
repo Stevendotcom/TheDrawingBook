5aec4b31e7534ad8ca667e1063fcaa9f62799c24
Alex Gallardo <gallardonicolas.8100@outlook.com>
2024-10-06 14:23:26 -0300

    add main menu

--- extras/scene_manager/main_menu.gd
+++ extras/scene_manager/main_menu.gd
@@ -6,3 +6,15 @@ func _process(_delta: float) -> void:
 
 func _on_play_button_up() -> void:
 	SceneManager.load_scene("res://scenes/zones/zone1.tscn")
+
+
+func _on_options_button_button_up() -> void:
+	pass # Replace with function body.
+
+
+func _on_credits_button_button_up() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_button_up() -> void:
+	pass # Replace with function body.
