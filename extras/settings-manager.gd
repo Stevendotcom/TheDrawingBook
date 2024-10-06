31a79e8db06c7d88391468256a7e33524d4175e0
Nicolas Leon <nicolasaleon05@gmail.com>
2024-10-06 20:44:07 -0300

    Add screen mode change buttons and sound slider

--- extras/settings-manager.gd
+++ extras/settings-manager.gd
@@ -5,13 +5,7 @@ enum Axis {
 	HEIGHT,
 }
 
-const screen_sizes = {
-	"large": [1920, 1080], # common for Full HD
-	"mid": [1366, 768], # common for laptops
-	"small": [1024, 576], # common for smaller devices
-};
-
-const volume_def: float = 5
+const volume_def: float = 100
 const screen_width_def: int = 1920
 const screen_height_def: int = 1080
 const fullscreen_def: bool = false
@@ -26,24 +20,53 @@ const max_vol: int = 10
 func change_vol(newVol: float) -> void:
 	volume = newVol
 
-func change_screen_size(screen_size: String) -> void:
-	if screen_sizes.find_key(screen_size):
-		screen_width = screen_sizes[screen_size][Axis.WIDTH]
-		screen_height = screen_sizes[screen_size][Axis.HEIGHT]
-
-
-func toggle_fullscreen() ->void:
-	fullscreen = !fullscreen
+#This are not used with the current menu
+#func toggle_fullscreen() ->void:
+#	fullscreen = !fullscreen
+#const screen_sizes = {
+#	"large": [1920, 1080], # common for Full HD
+#	"mid": [1366, 768], # common for laptops
+#	"small": [1024, 576], # common for smaller devices
+#};
+#func change_screen_size(screen_size: String) -> void:
+#	if screen_sizes.find_key(screen_size):
+#		screen_width = screen_sizes[screen_size][Axis.WIDTH]
+#		screen_height = screen_sizes[screen_size][Axis.HEIGHT]
 
 func update_settings() -> void:
 	ProjectSettings.set_setting("display/window/size/viewport_height", screen_height)
 	ProjectSettings.set_setting("display/window/size/viewport_width", screen_width)
-	ProjectSettings.set_setting(
-		"", 
-		DisplayServer.WindowMode.WINDOW_MODE_FULLSCREEN if fullscreen 
-		else DisplayServer.WindowMode.WINDOW_MODE_MAXIMIZED
-		)
+	
+	if fullscreen:
+		DisplayServer.window_set_mode(DisplayServer.WindowMode.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WindowMode.WINDOW_MODE_MAXIMIZED)
+	
+	#This doesn't work. The lines above this do
+	#ProjectSettings.set_setting(
+	#"display/window/size/window_mode",
+	#DisplayServer.WindowMode.WINDOW_MODE_FULLSCREEN if fullscreen else DisplayServer.WindowMode.WINDOW_MODE_WINDOWED
+	#)
+	
 	AudioServer.set_bus_volume_db(
 			AudioServer.get_bus_index("Master"),
 			linear_to_db(volume)
 		)
+		
+	ProjectSettings.save()
+
+
+func _on_full_screen_pressed() -> void:
+	print("FullScreen pressed")
+	fullscreen = true
+	update_settings()
+
+func _on_window_mode_pressed() -> void:
+	print("WindowMode pressed")
+	fullscreen = false
+	update_settings()
+
+func _on_volume_slider_value_changed(value: float) -> void:
+	print("Slider moved, new value: ", value)
+	change_vol(value)
+	update_settings()
